00aafea4fbe29d998342fd3c227e5e027061dfed
Tien Dzung <dung@serikb.com>
2023-11-07 18:40:12 +0700

    Update Ad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,17 @@
+import deps.dependOn
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
     namespace = "com.solid.test"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.solid.test"
@@ -27,22 +33,30 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget =  JavaVersion.VERSION_17.toString()
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = deps.Compose.Versions.composeCompiler
     }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("io.github.tiendung717:chipmango:0.0.2")
+    implementation(project(":chipmango-ad"))
+    dependOn(
+        deps.AndroidX,
+        deps.Compose,
+        deps.Hilt,
+        deps.Log,
+        deps.Ads
+    )
 }
--- app/src/main/java/com/chipmango/app/MainActivity.kt
+++ app/src/main/java/com/chipmango/app/MainActivity.kt
@@ -1,12 +1,53 @@
 package com.chipmango.app
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.chipmango.log.SolidLog
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import io.chipmango.ad.AdUnit
+import io.chipmango.ad.interstitial.rememberInterstitialAd
+
+data object InterstitialAdUnit : AdUnit("")
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SolidLog().check()
+        setContent {
+            var reloadAd by remember {
+                mutableLongStateOf(0L)
+            }
+            val interstitialAd by rememberInterstitialAd(
+                key = reloadAd,
+                context = LocalContext.current,
+                adUnit = InterstitialAdUnit,
+                isTestAd = true,
+                isPremium = false,
+                onAdClosed = {
+                    reloadAd = System.currentTimeMillis()
+                },
+                onAdLoadFailed = {
+
+                }
+            )
+            Box(Modifier.fillMaxSize()) {
+                Button(
+                    onClick = {
+                        interstitialAd?.show(this@MainActivity)
+                    }
+                ) {
+                    Text("Show")
+                }
+            }
+        }
     }
 }
