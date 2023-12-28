7800ea98398e8cd5a47b4d00b9be7bd781539cff
Dzung <android02@solidwaretech.com>
2023-12-28 15:36:55 +0700

    Add RevenueCat

---
+++ chipmango-revenue-cat/src/main/java/io/chipmango/revenuecat/PurchaseListener.kt
@@ -0,0 +1,12 @@
+package io.chipmango.revenuecat
+
+import com.revenuecat.purchases.CustomerInfo
+import com.revenuecat.purchases.Package
+import com.revenuecat.purchases.PurchasesError
+
+interface PurchaseListener {
+    fun onPurchaseStarted(packageToPurchase: Package)
+    fun onPurchaseCompleted(customerInfo: CustomerInfo)
+    fun onPurchaseCancelled()
+    fun onPurchaseError(error: PurchasesError)
+}
