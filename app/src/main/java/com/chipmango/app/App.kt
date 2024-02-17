4f9b7fe32e6c5a279488feec77af0abb71c87c6e
Tien Dzung <android02@solidwaretech.com>
2024-02-17 21:12:07 +0700

    Update test app

--- app/src/main/java/com/chipmango/app/App.kt
+++ app/src/main/java/com/chipmango/app/App.kt
@@ -2,14 +2,22 @@ package com.chipmango.app
 
 import dagger.hilt.android.HiltAndroidApp
 import io.chipmango.base.CoreApplication
+import io.chipmango.revenuecat.RevenueCat
+import javax.inject.Inject
 
 @HiltAndroidApp
 class App : CoreApplication() {
+
+
+    @Inject
+    lateinit var revenueCat: RevenueCat
+
     override fun isLogEnabled(): Boolean {
         return true
     }
 
     override fun onCreate() {
         super.onCreate()
+        revenueCat.init(this, "goog_pyAPqDZUVyHHDXMlcLPUvNWgPQo", true)
     }
 }
