ebd550d3b4187bcdd4a54d6ae78a9b648374f35d
Tien Dzung <android02@solidwaretech.com>
2024-02-17 21:59:33 +0700

    Update 0.3.5

--- buildSrc/src/main/java/deps/RevenueCat.kt
+++ buildSrc/src/main/java/deps/RevenueCat.kt
@@ -1,8 +1,8 @@
 package deps
 
 object RevenueCat : Dependency() {
-    private const val revenueCat = "com.revenuecat.purchases:purchases:7.3.1"
-    private const val revenueCatUi = "com.revenuecat.purchases:purchases-ui:7.3.1"
+    private const val revenueCat = "com.revenuecat.purchases:purchases:7.5.2"
+    private const val revenueCatUi = "com.revenuecat.purchases:purchases-ui:7.5.2"
 
     override fun implementations() = listOf<String>(
         revenueCat,
