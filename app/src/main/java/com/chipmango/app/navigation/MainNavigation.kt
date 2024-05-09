59952ea06580fc547918e89eeabb5dfbfd3ca30a
Tien <tiendt8@gmail.com>
2024-05-09 10:24:27 +0700

    [chipmango-core][0.5.1]

--- app/src/main/java/com/chipmango/app/navigation/MainNavigation.kt
+++ app/src/main/java/com/chipmango/app/navigation/MainNavigation.kt
@@ -2,18 +2,17 @@ package com.chipmango.app.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.core.os.bundleOf
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.chipmango.app.ui.ScreenTestAccount
 import com.chipmango.app.ui.ScreenTestHome
 import io.chipmango.navigation.destination.destination
-import io.chipmango.navigation.navigator.Navigator
+import io.chipmango.navigation.navigator.Navigation
 import io.chipmango.navigation.navigator.getRequiredLong
 import io.chipmango.navigation.navigator.navigate
 
 @Composable
 fun MainNavigation(navController: NavHostController) {
-    Navigator(
+    Navigation(
         navController = navController,
         startingDestination = Screens.TestHome
     ) {
--- chipmango/src/main/java/io/chipmango/navigation/navigator/Navigation.kt
+++ chipmango/src/main/java/io/chipmango/navigation/navigator/Navigation.kt
@@ -1,26 +1,37 @@
 package io.chipmango.navigation.navigator
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.util.Consumer
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import io.chipmango.navigation.destination.Destination
 
 @Composable
-fun Navigator(
+fun Navigation(
     modifier: Modifier = Modifier,
     navController: NavHostController,
     startingDestination: Destination,
+    enterTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition) =
+        { fadeIn(animationSpec = tween(700)) },
+    exitTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition) =
+        { fadeOut(animationSpec = tween(700)) },
+    popEnterTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition) =
+        enterTransition,
+    popExitTransition: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition) =
+        exitTransition,
     builder: NavGraphBuilder.() -> Unit
 ) {
     val activity = LocalContext.current as AppCompatActivity
@@ -39,17 +50,9 @@ fun Navigator(
         navController = navController,
         startDestination = startingDestination.route,
         builder = builder,
-        enterTransition = {
-            fadeIn()
-        },
-        exitTransition = {
-            fadeOut()
-        },
-        popEnterTransition = {
-            fadeIn()
-        },
-        popExitTransition = {
-            fadeOut()
-        }
+        enterTransition = enterTransition,
+        exitTransition = exitTransition,
+        popEnterTransition = popEnterTransition,
+        popExitTransition = popExitTransition
     )
 }
